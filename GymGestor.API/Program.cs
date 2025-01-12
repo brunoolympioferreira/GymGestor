f085403049d98493109069c5f527c1c47dbecd54
Bruno Ferreira <bruno.olympio.ferreira@gmail.com>
2025-01-12 09:17:22 -0300

    feat: adiciona configuraçoes

--- GymGestor.API/Program.cs
+++ GymGestor.API/Program.cs
@@ -1,12 +1,22 @@
+using GymGestor.API.Filters;
 using GymGestor.Infra;
 using Serilog;
 using Serilog.Sinks.MSSqlServer;
-using Serilog.Sinks.MSSqlServer.Sinks.MSSqlServer.Options;
+using System.Text.Json.Serialization;
 
 var builder = WebApplication.CreateBuilder(args);
 
-builder.Services.AddControllers();
+builder.Services.AddRouting(options => options.LowercaseUrls = true);
+
+builder.Services.AddControllers().AddJsonOptions(x =>
+{
+    x.JsonSerializerOptions.Converters.Add(new JsonStringEnumConverter());
+});
+
+builder.Services.AddMvc(options => options.Filters.Add(typeof(ExceptionsFilter)));
+
 builder.Services.AddEndpointsApiExplorer();
+
 builder.Services.AddSwaggerGen();
 
 builder.Services.AddInfra();
