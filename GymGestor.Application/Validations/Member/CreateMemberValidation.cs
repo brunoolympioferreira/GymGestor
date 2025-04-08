4575ab224a443f506247b94860fa8710cdc95956
Bruno Ferreira <bruno.olympio.ferreira@gmail.com>
2025-04-08 16:10:06 -0300

    feat: adiciona dtos e classes de validacoes

---
+++ GymGestor.Application/Validations/Member/CreateMemberValidation.cs
@@ -0,0 +1,56 @@
+﻿using FluentValidation;
+using GymGestor.Application.Models.InputModels.Member;
+using GymGestor.Application.Validations.Shared;
+
+namespace GymGestor.Application.Validations.Member
+{
+    public class CreateMemberValidation : AbstractValidator<CreateMemberInputModel>
+    {
+        private const int FULL_NAME_MAX_LENGHT = 150;
+        private const int GENDER_MAX_LENGHT = 50;
+        private const int CPF_MAX_LENGHT = 14;
+        private const int EMAIL_MAX_LENGHT = 150;
+        private const int PHONE_MAX_LENGHT = 20;
+        private const int PHOTO_MAX_LENGHT = 255;
+        public CreateMemberValidation()
+        {
+            RuleFor(m => m.FullName)
+                .NotNull().WithMessage("Nome do membro não pode ser nulo")
+                .NotEmpty().WithMessage("Nome do membro não pode ser vazio")
+                .MaximumLength(FULL_NAME_MAX_LENGHT).WithMessage($"Nome do membro deve conter no máximo {FULL_NAME_MAX_LENGHT} caracteres");
+
+            RuleFor(m => m.DateOfBirth)
+                .NotNull().WithMessage("Data de Nascimento do membro não pode ser nulo")
+                .NotEmpty().WithMessage("Data de Nascimento do membro não pode ser vazio");
+
+            RuleFor(m => m.Gender)
+                .NotNull().WithMessage("Gênero do membro não pode ser nulo")
+                .NotEmpty().WithMessage("Gênero do membro não pode ser vazio")
+                .MaximumLength(GENDER_MAX_LENGHT).WithMessage($"Gênero deve conter no máximo {GENDER_MAX_LENGHT} caracteres");
+
+            RuleFor(m => m.CPF)
+                .NotNull().WithMessage("CPF do membro não pode ser nulo")
+                .NotEmpty().WithMessage("CPF do membro não pode ser vazio")
+                .MaximumLength(CPF_MAX_LENGHT).WithMessage($"CPF deve conter no máximo {CPF_MAX_LENGHT} caracteres");
+
+            RuleFor(m => m.Email)
+                .NotNull().WithMessage("Email do membro não pode ser nulo")
+                .NotEmpty().WithMessage("Email do membro não pode ser vazio")
+                .EmailAddress().WithMessage("Email inválido")
+                .MaximumLength(EMAIL_MAX_LENGHT).WithMessage($"Email deve conter no máximo {EMAIL_MAX_LENGHT} caracteres");
+
+            RuleFor(m => m.Phone)
+                .NotNull().WithMessage("Telefone do membro não pode ser nulo")
+                .NotEmpty().WithMessage("Telefone do membro não pode ser vazio")
+                .MaximumLength(PHONE_MAX_LENGHT).WithMessage($"Telefone deve conter no máximo {PHONE_MAX_LENGHT} caracteres");
+
+            RuleFor(m => m.Address)
+                .SetValidator(new AddressDTOValidator());
+
+            RuleFor(m => m.PhotoUrl)
+                .NotNull().WithMessage("Foto do membro não pode ser nulo")
+                .NotEmpty().WithMessage("Foto do membro não pode ser vazio")
+                .MaximumLength(PHOTO_MAX_LENGHT).WithMessage($"Telefone deve conter no máximo {PHOTO_MAX_LENGHT} caracteres");
+        }
+    }
+}
---
+++ GymGestor.Application/Validations/Shared/AddressDTOValidator.cs
@@ -0,0 +1,41 @@
+﻿using FluentValidation;
+using GymGestor.Application.Models.DTOs;
+
+namespace GymGestor.Application.Validations.Shared
+{
+    public class AddressDTOValidator : AbstractValidator<AddressDTO>
+    {
+        private const int STREET_MAX_LENGHT = 200;
+        private const int NUMBER_MAX_LENGHT = 10;
+        private const int CITY_MAX_LENGHT = 100;
+        private const int STATE_MAX_LENGHT = 50;
+        private const int ZIPCODE_MAX_LENGHT = 20;
+        public AddressDTOValidator()
+        {
+            RuleFor(a => a.Street)
+                .NotNull().WithMessage("Rua não pode ser nulo")
+                .NotEmpty().WithMessage("Rua não pode ser vazio")
+                .MaximumLength(STREET_MAX_LENGHT);
+
+            RuleFor(a => a.Number)
+                .NotNull().WithMessage("Número não pode ser nulo")
+                .NotEmpty().WithMessage("Número não pode ser vazio")
+                .MaximumLength(NUMBER_MAX_LENGHT);
+
+            RuleFor(a => a.City)
+                .NotNull().WithMessage("Cidade não pode ser nulo")
+                .NotEmpty().WithMessage("Cidade não pode ser vazio")
+                .MaximumLength(CITY_MAX_LENGHT);
+
+            RuleFor(a => a.State)
+                .NotNull().WithMessage("Estado não pode ser nulo")
+                .NotEmpty().WithMessage("Estado não pode ser vazio")
+                .MaximumLength(STATE_MAX_LENGHT);
+
+            RuleFor(a => a.ZipCode)
+                .NotNull().WithMessage("CEP não pode ser nulo")
+                .NotEmpty().WithMessage("CEP não pode ser vazio")
+                .MaximumLength(ZIPCODE_MAX_LENGHT);
+        }
+    }
+}
