7dec45c74374ce84b668484fb8a60878e6733f93
Bruno Ferreira <bruno.olympio.ferreira@gmail.com>
2025-04-12 17:27:59 -0300

    feat: implementa testes unitarios para criar um membro

---
+++ GymGestor.API/Controllers/MembersController.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace GymGestor.API.Controllers;
+[Route("api/[controller]")]
+[ApiController]
+public class MembersController : ControllerBase
+{
+}
