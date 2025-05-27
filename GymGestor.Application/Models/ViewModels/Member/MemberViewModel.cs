8333fbc83b9d2f1d0cfce98c093f5ee7ad403e49
Bruno Ferreira <bruno.olympio.ferreira@gmail.com>
2025-05-27 14:39:04 -0300

    fix: modifica visualizacao da model

--- GymGestor.Application/Models/ViewModels/Member/MemberViewModel.cs
+++ GymGestor.Application/Models/ViewModels/Member/MemberViewModel.cs
@@ -4,8 +4,9 @@ public class MemberViewModel
     public Guid Id { get; set; }
     public string? FullName { get; set; }
     public string? Email { get; set; }
+    public string? CPF { get; set; }
     public string? Phone { get; set; }
-    public string? PhotoUrl { get; set; }
+
 
     public static MemberViewModel FromEntity(Core.Entities.Member member)
     {
@@ -14,8 +15,8 @@ public class MemberViewModel
             Id = member.Id,
             FullName = member.FullName,
             Email = member.Email.Value,
+            CPF = member.CPF.Value,
             Phone = member.Phone,
-            PhotoUrl = member.PhotoUrl,
         };
     }
 }
