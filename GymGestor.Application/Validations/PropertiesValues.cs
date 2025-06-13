76153c23816dc72c2cfdbc2a9c364c2773f362e1
Bruno Ferreira <bruno.olympio.ferreira@gmail.com>
2025-06-13 17:48:48 -0300

    fix: aumenta limite do campo url

--- GymGestor.Application/Validations/PropertiesValues.cs
+++ GymGestor.Application/Validations/PropertiesValues.cs
@@ -6,7 +6,7 @@ public static class PropertiesValues
     public readonly static int CPFMaxLenght = 14;
     public readonly static int EmailMaxLenght = 150;
     public readonly static int PhoneMaxLenght = 20;
-    public readonly static int PhotoMaxLenght = 255;
+    public readonly static int PhotoMaxLenght = 1000;
     public readonly static int ConditionMaxLenght = 500;
     public readonly static int NotesMaxLenght = 1000;
 
